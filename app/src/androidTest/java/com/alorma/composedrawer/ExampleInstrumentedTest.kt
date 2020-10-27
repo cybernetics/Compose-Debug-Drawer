4478db1dec5e9097d077137bee69c798760bf94d
Bernat Borrás <bernat.borras@adevinta.com>
2020-10-27 10:03:56 +0100

    Rollback gradle files

--- app/src/androidTest/java/com/alorma/composedrawer/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/alorma/composedrawer/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.alorma.compsedrawer", appContext.packageName)
+        assertEquals("com.alorma.composedrawer", appContext.packageName)
     }
 }
